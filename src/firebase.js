f59f40c6ed946b0bdfbba86f0699ea5648be0514
Ralph Cacho <cacho.rc@gmail.com>
2018-04-13 22:27:29 -0700

    Add SignUp via Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyCRkR4myiOdD1LLZpLmY-Avxis000cT0TE",
+  authDomain: "goal-coach-cccb4.firebaseapp.com",
+  databaseURL: "https://goal-coach-cccb4.firebaseio.com",
+  projectId: "goal-coach-cccb4",
+  storageBucket: "",
+  messagingSenderId: "743018077019"
+}
+
+export const firebaseApp = firebase.initializeApp(config)
