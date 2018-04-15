0af435bca6868f8a5558dc93c793030a5e1a6496
Ralph Cacho <cacho.rc@gmail.com>
2018-04-14 19:09:48 -0700

    Add sign out button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { firebaseApp } from '../firebase';
 
-export default class App extends React.Component{
- render(){
-  return(
-    <div>
-      <h1>I'm the App component</h1>
-    </div>
-  );
- }
+export default class App extends React.Component {
+  signOut() {
+    firebaseApp.auth().signOut()
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>I'm the App component</h1>
+        <button
+          className="btn btn-danger"
+          onClick={() => this.signOut()}
+        >
+          Sign Out
+        </button>
+      </div>
+    );
+  }
 }
