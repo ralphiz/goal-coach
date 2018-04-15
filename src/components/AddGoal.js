4484f6b893791b481ed43cb8297473df280a7408
Ralph Cacho <cacho.rc@gmail.com>
2018-04-15 03:19:45 -0700

    Create AddGoal component
    Logs to console for now.

---
+++ src/components/AddGoal.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class AddGoal extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    }
+  }
+
+  addGoal() {
+    console.log('this.state', this.state)
+  }
+
+  render() {
+    return (
+      <div className="form-inline">
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Add a goal"
+            className="form-control"
+            style={{ marginRight: '5px' }}
+            onChange={ event => this.setState({title: event.target.value}) }
+          />
+          <button
+            className="btn btn-success"
+            type="button"
+            onClick={() => this.addGoal()}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
