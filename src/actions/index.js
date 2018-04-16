cd76ee9eec7f43417c4a44605e1b91a9bbc52136
Ralph Cacho <cacho.rc@gmail.com>
2018-04-16 10:45:31 -0700

    Add constant and action for set goals

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,18 @@
-import { SIGNED_IN } from '../constants';
+import { SIGNED_IN, SET_GOALS } from '../constants';
 
 export function logUser(email) {
   const action = {
     type: SIGNED_IN,
     email
+  }
+
+  return action;
+}
+
+export function setGoals(goals) {
+  const action = {
+    type: SET_GOALS,
+    goals
   }
 
   return action;
