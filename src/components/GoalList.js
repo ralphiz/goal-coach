9820776b87c178e3e0312febfcce372983aceda8
Ralph Cacho <cacho.rc@gmail.com>
2018-04-16 10:55:19 -0700

    Display the list of goals

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { goalRef } from '../firebase';
 import { setGoals } from '../actions';
+import GoalItem from './GoalItem';
 
 class GoalList extends Component {
   componentDidMount() {
@@ -20,7 +21,13 @@ class GoalList extends Component {
     console.log('this.props.goals', this.props.goals);
     return (
       <div>
-        
+        {
+          this.props.goals.map((goal, index) => {
+            return (
+              <GoalItem key={index} goal={goal} />
+            )
+          })
+        }
       </div>
     );
   }
