acb6dd993c9378feb572cddce2f27ce6202b15f4
Ralph Cacho <cacho.rc@gmail.com>
2018-04-16 00:56:55 -0700

    Create and use GoalList component

---
+++ src/components/GoalList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { goalRef } from '../firebase';
+
+class GoalList extends Component {
+  componentDidMount() {
+    goalRef.on('value', snap => {
+      let goals = [];
+      snap.forEach(goal => {
+        const { email, title } = goal.val();
+        goals.push({ email, title });
+      })
+      console.log('goals', goals);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default GoalList;
