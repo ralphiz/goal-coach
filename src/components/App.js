bf36a0dd0c533e661b576fc6185280bb46190e87
Ralph Cacho <cacho.rc@gmail.com>
2018-04-16 13:34:40 -0700

    Create Complete Goals component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { firebaseApp } from '../firebase';
 import AddGoal from './AddGoal';
 import GoalList from './GoalList';
+import CompleteGoalList from './CompleteGoalList';
 
 class App extends Component {
   signOut() {
@@ -15,8 +16,14 @@ class App extends Component {
         <h3>Goal Coach</h3>
         <AddGoal />
         <hr />
+        
         <h4>Goals</h4>
         <GoalList />
+        
+        <hr />
+        <h4>Complete Goals</h4>
+        <CompleteGoalList />
+        
         <hr />
         <button
           className="btn btn-danger"
